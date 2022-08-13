9c2c0082410b7c38161b345abade8ffe6c12edfe
Dominik Wernberger <dominik2.wernberger@tum.de>
2022-08-13 13:16:22 +0200

    Run tests in parallel

---
+++ run_parallel_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+test=build/test/test
+
+$test --list-tests --verbosity quiet |
+    awk '{print "\042" $0 "\042"}' |
+    parallel --bar --halt now,fail=1 -j $(nproc) $test "{}" ">" "test.log"
+
+# $test --list-tests --verbosity quiet |
+#     awk '{print "\042" $0 "\042"}' |
+#     parallel --bar -j $(nproc) $test "{}" ">>" "test.log"
