71a6a64bddcb0769cf078c44724766e5ae0ee1fe
Dominik Wernberger <dominik2.wernberger@tum.de>
2022-08-18 23:52:30 +0200

    Update build script

--- build_and_test.sh
+++ build_and_test.sh
@@ -3,23 +3,24 @@
 echo Downloading/Setting Up Unit Test Repositories...
 
 # Download repositories for unit tests
-python3 test/py/testing.py -r True -d repos.yaml -t thirdparty
+python3 test/py/testing.py -r True -d repos.yaml -t test/designs
 
 echo Generating Unit Tests...
 
 # Generate C++ unit test files
-python3 test/py/testing.py -g 1 -d repos.yaml -u test/src/Misc -t thirdparty
+python3 test/py/testing.py -g 1 -d repos.yaml -u test/src/generated_tests -t test/designs
 
 echo Building Applications...
 
 # Build applications
 cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=../vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+cmake --build build -j $(nproc)
 
 echo Running Unit Tests...
 
 # Run unit tests
-./build/test/test > OpenOrCadParser.log 2>&1 && echo OK
+./build/test/test > OpenOrCadParser.log 2>&1
+# ./run_parallel_tests.sh
 
 echo Writing Back Error Counter Log into Database...
 
