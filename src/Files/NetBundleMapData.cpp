b6395fade40be55fb60be607c1fe9c18ffbf5bee
Dominik Wernberger <dominik2.wernberger@tum.de>
2022-08-18 16:57:10 +0200

    Add AdminData and NetBundleMapData parsers, but do not parse yet

---
+++ src/Files/NetBundleMapData.cpp
@@ -0,0 +1,88 @@
+#include <cstdint>
+#include <string>
+
+#include <nameof.hpp>
+#include <spdlog/spdlog.h>
+
+#include "../General.hpp"
+
+#include "NetBundleMapData.hpp"
+
+#include "../Parser.hpp"
+
+
+void Parser::readNetBundleMapData(const fs::path& aFilePath)
+{
+    // @todo probably move all this mFileCtr and try-catch block
+    //       stuff into a separate function
+    ++mFileCtr;
+    try
+    {
+        openFile(aFilePath);
+
+        spdlog::debug(getOpeningMsg(__func__, mDs.getCurrentOffset()));
+
+        NetBundleMapData obj;
+
+        mDs.discardBytes(2);
+
+        uint16_t number_groups = mDs.readUint16();
+
+        spdlog::debug("number_groups = {}", number_groups);
+
+        for(size_t i = 0U; i < number_groups; ++i)
+        {
+            spdlog::debug("[{}]:", i);
+
+            std::string group_name = mDs.readStringLenZeroTerm();
+            spdlog::debug("group_name = {}:", group_name);
+
+            Structure structure = read_type_prefix();
+
+            if(static_cast<int>(structure) != 0x67)
+            {
+                throw std::runtime_error(fmt::format("Expected 0x67 but got {}", static_cast<int>(structure)));
+            }
+
+            readPreamble();
+
+            mDs.assumeData({0x00, 0x00, 0x00, 0x00, 0x00, 0x00}, std::string(__func__) + " - 0");
+
+            // The following contains the information of a net group but
+            // the name of this group is stated outside. How should this
+            // be handled?
+
+            uint16_t number_group_elements = mDs.readUint16();
+
+            for(size_t j = 0U; j < number_group_elements; ++j)
+            {
+                std::string element_name = mDs.readStringLenZeroTerm();
+                spdlog::debug("  [{}]: element_name = {}", j, element_name);
+
+                // @todo 0x01 is probably a scalar wire
+                //       0x02 is probably a bus
+                uint16_t wire_type = mDs.readUint16();
+                spdlog::debug("       wire_type = {}", wire_type == 0x01 ? "Scalar" : wire_type == 0x02 ? "Bus" : "Unknown");
+            }
+
+            spdlog::debug(getClosingMsg(__func__, mDs.getCurrentOffset()));
+            spdlog::info(to_string(obj));
+
+            // return obj;
+        }
+
+        // @todo use functino
+        if(!mDs.isEoF())
+        {
+            throw std::runtime_error("Exptected EoF in NetBundleMapData");
+        }
+
+        closeFile();
+    }
+    catch(...)
+    {
+        exceptionHandling();
+    }
+
+    spdlog::info("\n----------------------------------------------------------------------------------\n");
+}
