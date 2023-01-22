9a3c1b9f1387de9eb9c7541cdd08eb3b28d027a2
Dominik Wernberger <dominik.wernberger@gmx.de>
2023-01-22 10:01:12 +0100

    StreamPage: Save some structures

--- src/Streams/StreamPage.hpp
+++ src/Streams/StreamPage.hpp
@@ -2,7 +2,9 @@
 #define STREAMPAGE_HPP
 
 
+#include <memory>
 #include <string>
+#include <vector>
 
 #include <fmt/core.h>
 #include <nameof.hpp>
@@ -10,6 +12,11 @@
 #include "CommonBase.hpp"
 #include "General.hpp"
 #include "PageSettings.hpp"
+#include "Structures/StructPartInst.hpp"
+#include "Structures/StructPort.hpp"
+#include "Structures/StructT0x34.hpp"
+#include "Structures/StructT0x35.hpp"
+#include "Structures/StructWire.hpp"
 
 
 class StreamPage : public CommonBase
@@ -26,6 +33,12 @@ public:
     std::string  name;
     std::string  pageSize;
     PageSettings pageSettings;
+
+    std::vector<std::unique_ptr<StructT0x34>>    t0x34s;
+    std::vector<std::unique_ptr<StructT0x35>>    t0x35s;
+    std::vector<std::unique_ptr<StructWire>>     wires;
+    std::vector<std::unique_ptr<StructPartInst>> partInsts;
+    std::vector<std::unique_ptr<StructPort>>     ports;
 };
 
 
@@ -39,6 +52,36 @@ static std::string to_string(const StreamPage& aObj)
     str += fmt::format("{}pageSize = {}\n", indent(1), aObj.pageSize);
     str += fmt::format("{}pageSettings:\n", indent(1));
     str += indent(aObj.pageSettings.to_string(), 2);
+
+    str += fmt::format("{}t0x34s:\n", indent(1));
+    for(size_t i = 0u; i < aObj.t0x34s.size(); ++i)
+    {
+        str += indent(fmt::format("[{}]: {}", i, aObj.t0x34s[i]->to_string()), 2);
+    }
+
+    str += fmt::format("{}t0x35s:\n", indent(1));
+    for(size_t i = 0u; i < aObj.t0x35s.size(); ++i)
+    {
+        str += indent(fmt::format("[{}]: {}", i, aObj.t0x35s[i]->to_string()), 2);
+    }
+
+    str += fmt::format("{}wires:\n", indent(1));
+    for(size_t i = 0u; i < aObj.wires.size(); ++i)
+    {
+        str += indent(fmt::format("[{}]: {}", i, aObj.wires[i]->to_string()), 2);
+    }
+
+    str += fmt::format("{}partInsts:\n", indent(1));
+    for(size_t i = 0u; i < aObj.partInsts.size(); ++i)
+    {
+        str += indent(fmt::format("[{}]: {}", i, aObj.partInsts[i]->to_string()), 2);
+    }
+
+    str += fmt::format("{}ports:\n", indent(1));
+    for(size_t i = 0u; i < aObj.ports.size(); ++i)
+    {
+        str += indent(fmt::format("[{}]: {}", i, aObj.ports[i]->to_string()), 2);
+    }
 
     return str;
 }
