8685d49eb1223eaf5bd0302d3575b7845286f2aa
Dominik Wernberger <dominik.wernberger@gmx.de>
2023-01-22 20:52:27 +0100

    StructSymbolPin: PinBus and PinScalar seem to use the same implementation

--- src/Structures/StructSymbolPinBus.hpp
+++ src/Structures/StructSymbolPinBus.hpp
@@ -14,65 +14,17 @@
 #include "Structures/StructSymbolPin.hpp"
 
 
-// @todo this looks exactly the same as SymbolPinScalar. Why is that?
 class StructSymbolPinBus : public StructSymbolPin
 {
 public:
 
-    StructSymbolPinBus(DataStream& aDs) : StructSymbolPin{aDs}, name{}, startX{0}, startY{0},
-        hotptX{0}, hotptY{0}, pinShape{}, portType{PortType::Input}
+    StructSymbolPinBus(DataStream& aDs) : StructSymbolPin{aDs}
     { }
 
-    std::string to_string() const override;
+    // std::string to_string() const override;
 
-    void read(FileFormatVersion aVersion = FileFormatVersion::Unknown) override;
-
-    std::string name;
-    int32_t startX;
-    int32_t startY;
-    int32_t hotptX;
-    int32_t hotptY;
-
-    PinShape pinShape;
-
-    PortType portType;
+    // void read(FileFormatVersion aVersion = FileFormatVersion::Unknown) override;
 };
-
-
-[[maybe_unused]]
-static std::string to_string(const StructSymbolPinBus& aObj)
-{
-    std::string str;
-
-    str += fmt::format("{}:\n", nameof::nameof_type<decltype(aObj)>());
-    str += fmt::format("{}name     = {}\n", indent(1), aObj.name);
-    str += fmt::format("{}startX   = {}\n", indent(1), aObj.startX);
-    str += fmt::format("{}startY   = {}\n", indent(1), aObj.startY);
-    str += fmt::format("{}hotptX   = {}\n", indent(1), aObj.hotptX);
-    str += fmt::format("{}hotptY   = {}\n", indent(1), aObj.hotptY);
-
-    str += fmt::format("{}pinShape:\n", indent(1));
-    str += indent(to_string(aObj.pinShape), 2);
-
-    str += fmt::format("{}portType = {}\n", indent(1), to_string(aObj.portType));
-
-    return str;
-}
-
-
-inline std::string StructSymbolPinBus::to_string() const
-{
-    return ::to_string(*this);
-}
-
-
-[[maybe_unused]]
-static std::ostream& operator<<(std::ostream& aOs, const StructSymbolPinBus& aVal)
-{
-    aOs << to_string(aVal);
-
-    return aOs;
-}
 
 
 #endif // STRUCTSYMBOLPINBUS_HPP
--- src/Structures/StructSymbolPinScalar.cpp
+++ src/Structures/StructSymbolPinScalar.cpp
@@ -11,46 +11,47 @@
 #include "Structures/StructSymbolPinScalar.hpp"
 
 
-void StructSymbolPinScalar::read(FileFormatVersion /* aVersion */)
-{
-    spdlog::debug(getOpeningMsg(getMethodName(this, __func__), mDs.get().getCurrentOffset()));
+// @note Moved to StructSymbolPin::read
+// void StructSymbolPinScalar::read(FileFormatVersion /* aVersion */)
+// {
+//     spdlog::debug(getOpeningMsg(getMethodName(this, __func__), mDs.get().getCurrentOffset()));
 
-    auto_read_prefixes();
+//     auto_read_prefixes();
 
-    readPreamble();
+//     readPreamble();
 
-    const std::optional<FutureData> thisFuture = getFutureData();
+//     const std::optional<FutureData> thisFuture = getFutureData();
 
-    name = mDs.get().readStringLenZeroTerm();
+//     name = mDs.get().readStringLenZeroTerm();
 
-    startX = mDs.get().readInt32();
-    startY = mDs.get().readInt32();
-    hotptX = mDs.get().readInt32();
-    hotptY = mDs.get().readInt32();
+//     startX = mDs.get().readInt32();
+//     startY = mDs.get().readInt32();
+//     hotptX = mDs.get().readInt32();
+//     hotptY = mDs.get().readInt32();
 
-    pinShape = ToPinShape(mDs.get().readUint16());
+//     pinShape = ToPinShape(mDs.get().readUint16());
 
-    mDs.get().printUnknownData(2, std::string(__func__) + " - 0");
+//     mDs.get().printUnknownData(2, std::string(__func__) + " - 0");
 
-    portType = ToPortType(mDs.get().readUint32());
+//     portType = ToPortType(mDs.get().readUint32());
 
-    mDs.get().printUnknownData(4, std::string(__func__) + " - 1");
+//     mDs.get().printUnknownData(4, std::string(__func__) + " - 1");
 
-    // @todo Type is StructSymbolDisplayProp
-    const uint16_t lenSymbolDisplayProps = mDs.get().readUint16();
+//     // @todo Type is StructSymbolDisplayProp
+//     const uint16_t lenSymbolDisplayProps = mDs.get().readUint16();
 
-    spdlog::trace("lenSymbolDisplayProps = {}", lenSymbolDisplayProps);
+//     spdlog::trace("lenSymbolDisplayProps = {}", lenSymbolDisplayProps);
 
-    for(size_t i = 0U; i < lenSymbolDisplayProps; ++i)
-    {
-        spdlog::critical("VERIFYING StructSymbolPinScalar Structure0 is {}", NAMEOF_TYPE_RTTI(*readStructure().get())); // @todo push structure
-        // readStructure();
-    }
+//     for(size_t i = 0U; i < lenSymbolDisplayProps; ++i)
+//     {
+//         spdlog::critical("VERIFYING StructSymbolPinScalar Structure0 is {}", NAMEOF_TYPE_RTTI(*readStructure().get())); // @todo push structure
+//         // readStructure();
+//     }
 
-    sanitizeThisFutureSize(thisFuture);
+//     sanitizeThisFutureSize(thisFuture);
 
-    readOptionalTrailingFuture();
+//     readOptionalTrailingFuture();
 
-    spdlog::debug(getClosingMsg(getMethodName(this, __func__), mDs.get().getCurrentOffset()));
-    spdlog::trace(to_string());
-}
+//     spdlog::debug(getClosingMsg(getMethodName(this, __func__), mDs.get().getCurrentOffset()));
+//     spdlog::trace(to_string());
+// }
