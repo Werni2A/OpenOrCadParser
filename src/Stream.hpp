1231f86cd1eec19bde1f23a131c03611ac65fad9
Dominik Wernberger <dominik.wernberger@gmx.de>
2023-12-30 16:31:51 +0100

    Move stream exception handling

--- src/Stream.hpp
+++ src/Stream.hpp
@@ -7,6 +7,10 @@
 #include <optional>
 #include <utility>
 
+#include <fmt/color.h>
+#include <fmt/core.h>
+#include <spdlog/spdlog.h>
+
 #include "ContainerContext.hpp"
 #include "Enums/Structure.hpp"
 #include "FutureData.hpp"
@@ -37,6 +41,27 @@ public:
         mCtx.mLogger.info("Closing file: {}", mCtx.mInputStream.string());
 
         mCtx.mDs.close();
+    }
+
+    void exceptionHandling()
+    {
+        try
+        {
+            throw;
+        }
+        catch(const std::exception& e)
+        {
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), "--------ERROR REPORT--------"));
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), "Input Container: {}", mCtx.mInputCfbfFile.string()));
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), "Current File:    {}", mCtx.mInputStream.string()));
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), mCtx.mDs.getCurrentOffsetStrMsg()));
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), "\nError Message: {}\n\n", e.what()));
+        }
+        catch(...)
+        {
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), "--------ERROR REPORT--------"));
+            mCtx.mLogger.error(fmt::format(fg(fmt::color::crimson), "Unknown exception caught!\n"));
+        }
     }
 
 // protected:
